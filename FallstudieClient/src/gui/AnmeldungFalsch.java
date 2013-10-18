15c1c7614859fddc6280445bdcb68bab1f8cac72
juli <juli@141.31.72.59>
2013-10-18 14:10:01 +0200

    commitbeauftragter

--- FallstudieClient/src/gui/AnmeldungFalsch.java
+++ FallstudieClient/src/gui/AnmeldungFalsch.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.awt.BorderLayout;
 
 import javax.swing.ImageIcon;
@@ -11,12 +12,11 @@ import javax.swing.JTextPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+
 @SuppressWarnings("serial")
 public class AnmeldungFalsch extends JDialog {
 	private final JPanel contentPanel = new JPanel();
-	/**
-	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		try {
 			AnmeldungFalsch dialog = new AnmeldungFalsch();
@@ -26,9 +26,7 @@ public class AnmeldungFalsch extends JDialog {
 			e.printStackTrace();
 		}
 	}
-	/**
-	 * Create the dialog.
-	 */
+
 	public AnmeldungFalsch() {
 		setTitle("Login - Fehlgeschlagen");
 		setResizable(false);
@@ -44,7 +42,6 @@ public class AnmeldungFalsch extends JDialog {
 			txtpnDieAnmeldungWar.setEditable(false);
 			txtpnDieAnmeldungWar.setBackground(new Color(255, 250, 240));
 			txtpnDieAnmeldungWar.setBounds(60, 10, 378, 75);
-			//TODO Inhalt
 			txtpnDieAnmeldungWar.setText("\n    Die Anmeldung war fehlerhaft. Versuchen Sie es erneut\n    oder wenden Sie sich an Ihren Systemadministrator.");
 			contentPanel.add(txtpnDieAnmeldungWar);
 		}
@@ -69,7 +66,7 @@ public class AnmeldungFalsch extends JDialog {
 			button.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					AnmeldungFalschHilfe frmAnmeldungFalschHilfe = new AnmeldungFalschHilfe();
-		        	frmAnmeldungFalschHilfe.setVisible(true); 
+					frmAnmeldungFalschHilfe.setVisible(true);
 				}
 			});
 		}
